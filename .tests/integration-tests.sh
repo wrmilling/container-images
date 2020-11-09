0f378b86bbcb9bbe09038b100c70c7445ea80058
Devin Buhl <devin.kray@gmail.com>
2020-11-09 08:06:49 -0500

    fix nzbhydra2 image

--- .tests/integration-tests.sh
+++ .tests/integration-tests.sh
@@ -11,8 +11,8 @@ verify () {
   echo "Image size: $(echo ${size} | numfmt --to=iec-i --suffix=B)"
 }
 
-version="$(curl -sX GET "https://api.github.com/repos/morpheus65535/bazarr/releases" | jq --raw-output '.[0].tag_name')"
-verify "bazarr" "${version}"
+# version="$(curl -sX GET "https://api.github.com/repos/morpheus65535/bazarr/releases" | jq --raw-output '.[0].tag_name')"
+# verify "bazarr" "${version}"
 # # docker run --entrypoint="" --rm kah-bazarr:0.9.0.5 python3 /app/bazarr.py -h
 
 # version=$(curl -sX GET "https://api.github.com/repos/Jackett/Jackett/releases/latest" | jq --raw-output '.tag_name')
@@ -27,8 +27,8 @@ verify "bazarr" "${version}"
 # verify "nzbget" "${version}"
 # # docker run --entrypoint="" --rm kah-nzbget:21.0 /app/nzbget --version
 
-# version=$(curl -sX GET "https://api.github.com/repos/theotherp/nzbhydra2/releases" | jq --raw-output '.[0].tag_name')
-# verify "nzbhydra2" "${version}"
+version=$(curl -sX GET "https://api.github.com/repos/theotherp/nzbhydra2/releases" | jq --raw-output '.[0].tag_name')
+verify "nzbhydra2" "${version}"
 # # docker run --entrypoint="" --rm kah-nzbhydra2:3.4.3 python3 /app/nzbhydra2wrapperPy3.py --version
 
 # version=$(curl -sX GET "https://api.github.com/repos/tidusjar/Ombi.Releases/releases" | jq --raw-output '.[0].tag_name')
