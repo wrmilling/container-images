5e81cccf5e7cd85c988a5a3712f6609d4df30f0c
Devin Buhl <devin.kray@gmail.com>
2020-11-10 08:33:49 -0500

    ubuntu: add shim

--- .tests/get-versions.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-find . -maxdepth 1 -mindepth 1 -type d | while read app; do
-  if test -f "./${app}/latest-version.sh"; then
-    version=$(bash "./${app}/latest-version.sh")
-    echo "${version}" | tee "${app}/.version" > /dev/null
-    echo "${app} ${version}"
-  fi
-done
