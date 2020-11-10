c0f1a303a9f9ec2f39062b667a7344d49f79dc1d
Devin Buhl <devin.kray@gmail.com>
2020-11-10 10:56:31 -0500

    update xteve latest-versions

--- xteve/latest-version.sh
+++ xteve/latest-version.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 version=$(curl -sX GET "https://api.github.com/repos/xteve-project/xTeVe/releases" | jq --raw-output '.[0].tag_name')
-echo "${version}"
+version="${version#*v}"
+version="${version#*release-}"
+printf "%s" "${version}"
