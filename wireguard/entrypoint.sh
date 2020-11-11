9e3d5f5780195bd7d217afc24f5b8d5c49ca839c
Bernd Schörgers <bernd@bjws.nl>
2020-11-11 15:13:32 +0100

    [wireguard] Add image (#5)

---
+++ wireguard/entrypoint.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ "$(cat /proc/sys/net/ipv4/conf/all/src_valid_mark)" != "1" ]]; then
+    echo "[WARNING] sysctl net.ipv4.conf.all.src_valid_mark=1 is not set" >&2
+fi
+
+CONFIGS=`sudo /usr/bin/find /etc/wireguard -type f -printf "%f\n"`
+if [[ -z "${CONFIGS}" ]]; then
+    echo "[ERROR] No configuration files found in /etc/wireguard" >&2
+    exit 1
+fi
+
+CONFIG=`echo $CONFIGS | head -n 1`
+INTERFACE="${CONFIG%.*}"
+
+sudo /usr/bin/wg-quick up "${INTERFACE}"
+
+source "/shim/killswitch.sh"
+
+_shutdown () {
+    echo "[INFO] Caught signal, shutting down VPN!"
+    sudo /usr/bin/wg-quick down "${INTERFACE}"
+}
+
+trap _shutdown SIGTERM SIGINT SIGQUIT
+
+sleep infinity &
+wait $!
---
+++ wireguard/shim/killswitch.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+KILLSWITCH=${KILLSWITCH:-"false"}
+SEPARATOR=${SEPARATOR:-";"}
+
+if
+    [[ "${KILLSWITCH}" == "true" ]];
+then
+    # IPv4 killswitch
+    DEFAULTROUTE_IPV4=$(/usr/sbin/ip -4 route | grep default | awk '{print $3}')
+    KILLSWITCH_EXCLUDEDNETWORKS_IPV4=${KILLSWITCH_EXCLUDEDNETWORKS_IPV4:-""}
+    sudo /usr/sbin/iptables -F OUTPUT
+    if 
+        [[ -n "${DEFAULTROUTE_IPV4}" ]] && [[ -n "$KILLSWITCH_EXCLUDEDNETWORKS_IPV4" ]];
+    then
+        IFS="${SEPARATOR}" read -r -a networks <<< "${KILLSWITCH_EXCLUDEDNETWORKS_IPV4}"
+        for entry in "${networks[@]}"
+        do
+            echo "[INFO] Excluding ${entry} from VPN IPv4 traffic"
+            sudo /usr/sbin/ip -4 route add "${entry}" via "${DEFAULTROUTE_IPV4}"
+            sudo /usr/sbin/iptables -A OUTPUT -d "${entry}" -j ACCEPT
+        done
+
+        sudo /usr/sbin/iptables -A OUTPUT ! -o "${INTERFACE}" -m mark ! --mark $(sudo /usr/bin/wg show "${INTERFACE}" fwmark) -m addrtype ! --dst-type LOCAL -j REJECT
+    fi
+
+    # IPv6 killswitch
+    DEFAULTROUTE_IPV6=$(/usr/sbin/ip -6 route | grep default | awk '{print $3}')
+    KILLSWITCH_EXCLUDEDNETWORKS_IPV6=${KILLSWITCH_EXCLUDEDNETWORKS_IPV6:-""}
+    sudo /usr/sbin/ip6tables -F OUTPUT
+    if 
+        [[ -n "${DEFAULTROUTE_IPV6}" ]] && [[ -n "${KILLSWITCH_EXCLUDEDNETWORKS_IPV6}" ]];
+    then
+        IFS="${SEPARATOR}" read -r -a networks <<< "${KILLSWITCH_EXCLUDEDNETWORKS_IPV6}"
+        for entry in "${networks[@]}"
+        do
+            echo "[INFO] Excluding ${entry} from VPN IPv6 traffic"
+            sudo /usr/sbin/ip -6 route add "${entry}" via "${DEFAULTROUTE_IPV6}"
+            sudo /usr/sbin/ip6tables -A OUTPUT -d "${entry}" -j ACCEPT
+        done
+
+        sudo /usr/sbin/ip6tables -A OUTPUT ! -o "${INTERFACE}" -m mark ! --mark $(sudo /usr/bin/wg show "${INTERFACE}" fwmark) -m addrtype ! --dst-type LOCAL -j REJECT
+    fi
+fi
