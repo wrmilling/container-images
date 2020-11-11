10b4a80ae50f14cd814a5f3eec92bae278943595
ᗪєνιη ᗷυнʟ <onedr0p@users.noreply.github.com>
2020-11-11 06:52:51 -0500

    [qbittorrent] add config file the correct way (#3)

--- qbittorrent/entrypoint.sh
+++ qbittorrent/entrypoint.sh
@@ -4,4 +4,9 @@
 source "/shim/umask.sh"
 source "/shim/vpn.sh"
 
+if [[ ! -f "/config/qBittorrent/qBittorrent.conf" ]]; then
+    mkdir -p /config/qBittorrent
+    cp /app/qBittorrent.conf /config/qBittorrent/qBittorrent.conf
+fi
+
 exec /app/qbittorrent-nox --webui-port="${WEBUI_PORT}" ${EXTRA_ARGS}
