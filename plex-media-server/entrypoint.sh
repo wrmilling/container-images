61a0aaef518d95795eb57d9f67023b2507fda088
Devin Buhl <devin.kray@gmail.com>
2020-11-16 12:15:34 -0500

    plex add pref shim

--- plex-media-server/entrypoint.sh
+++ plex-media-server/entrypoint.sh
@@ -3,6 +3,7 @@
 #shellcheck disable=SC1091
 source "/shim/umask.sh"
 source "/shim/vpn.sh"
+source "/shim/plex-preferences.sh"
 
 #shellcheck disable=SC2155
 export PLEX_MEDIA_SERVER_INFO_MODEL=$(uname -m)
