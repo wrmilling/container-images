6b982c71833af5e09656c3d8eaaa58c1d291a0f2
Devin Buhl <devin.kray@gmail.com>
2020-11-16 11:19:53 -0500

    plex ldpath in dockerfile

--- plex-media-server/entrypoint.sh
+++ plex-media-server/entrypoint.sh
@@ -9,4 +9,4 @@ export PLEX_MEDIA_SERVER_INFO_MODEL=$(uname -m)
 #shellcheck disable=SC2155
 export PLEX_MEDIA_SERVER_INFO_PLATFORM_VERSION=$(uname -r)
 
-exec /bin/bash -c 'LD_LIBRARY_PATH=/usr/lib/plexmediaserver:/usr/lib/plexmediaserver/lib /usr/lib/plexmediaserver/Plex\ Media\ Server'
+exec /usr/lib/plexmediaserver/Plex\ Media\ Server
