5038cbeeedb5285db05476aa1f099f98d5331eef
Devin Buhl <devin.kray@gmail.com>
2020-11-17 08:06:25 -0500

    actions-runner get version from dockerhub

--- actions-runner/latest-version.sh
+++ actions-runner/latest-version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-version="$(curl -sX GET "https://api.github.com/repos/actions/runner/releases" | jq --raw-output '.[0].tag_name')"
+version=$(curl -sX GET "https://registry.hub.docker.com/v1/repositories/summerwind/actions-runner/tags" | jq --raw-output 'last(.[].name)')
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
