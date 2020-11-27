071ca8014c743e6901ea0920f28434bf89a0a1a5
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <6213398+bjw-s@users.noreply.github.com>
2020-11-27 13:09:09 +0100

    [git-crypt] Add new image (#17)

---
+++ git-crypt/latest-version.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+version="$(curl -sX GET "https://api.github.com/repos/AGWA/git-crypt/tags" | jq --raw-output 'del(.[] | select(.name | contains ("debian")))[0].name')"
+version="${version#*v}"
+version="${version#*release-}"
+printf "%s" "${version}"
