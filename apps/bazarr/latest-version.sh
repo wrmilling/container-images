9fdde4a3d744002d1f7a7a3fdbff87a3d421b87d
Devin Buhl <devin.kray@gmail.com>
2021-03-15 13:57:51 -0400

    fix: don't build bazarr pre-releases

--- apps/bazarr/latest-version.sh
+++ apps/bazarr/latest-version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-version="$(curl -sX GET "https://api.github.com/repos/morpheus65535/bazarr/releases" | jq --raw-output '.[0].tag_name')"
+version="$(curl -sX GET "https://api.github.com/repos/morpheus65535/bazarr/releases/latest" | jq --raw-output '.tag_name')"
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
