7a63a70ab10adefdb2d9415f3771aa949610605b
ᗪєνιη ᗷυнʟ <onedr0p@users.noreply.github.com>
2021-04-11 11:02:07 -0400

    fix: pre-create plex app support dir

--- apps/plex/entrypoint.sh
+++ apps/plex/entrypoint.sh
@@ -10,4 +10,8 @@ export PLEX_MEDIA_SERVER_INFO_MODEL=$(uname -m)
 #shellcheck disable=SC2155
 export PLEX_MEDIA_SERVER_INFO_PLATFORM_VERSION=$(uname -r)
 
+if [ ! -d "${PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR}" ]; then
+    mkdir -p "${PLEX_MEDIA_SERVER_APPLICATION_SUPPORT_DIR}"
+fi
+
 exec /usr/lib/plexmediaserver/Plex\ Media\ Server
