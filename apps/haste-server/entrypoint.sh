9d7890c7f89cbd59001b3430248784aaf09144a7
nιcнolaѕ wιlde <ncwilde43@gmail.com>
2021-04-21 07:49:47 -0700

    [haste] Change source to upstream (#104)
    * - Changed source to upstream, seejohnrun.
    - Added greeting.sh to entrypoint.sh
    - Added all ENV variables.
    
    * Renamed haste to haste-server
    
    Signed-off-by: Nicholas Wilde <ncwilde43@gmail.com>
    
    * Removed greeting shim
    
    Signed-off-by: Nicholas Wilde <ncwilde43@gmail.com>

--- apps/haste-server/entrypoint.sh
+++ apps/haste-server/entrypoint.sh
@@ -4,4 +4,8 @@
 source "/shim/umask.sh"
 source "/shim/vpn.sh"
 
+set -e
+
+node ./docker-entrypoint.js > ./config.js
+
 exec node /app/server.js ${@} ${EXTRA_ARGS}
--- apps/haste-server/latest-version.sh
+++ apps/haste-server/latest-version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-version=$(curl -sX GET https://api.github.com/repos/zneix/haste-server/commits/master | jq --raw-output '. | .sha')
+version=$(curl -sX GET https://api.github.com/repos/seejohnrun/haste-server/commits/master | jq --raw-output '. | .sha')
 version="${version#*v}"
 version="${version#*release-}"
 version="${version:0:7}"
