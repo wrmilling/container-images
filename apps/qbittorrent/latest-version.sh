199dc53b113038e9af2673c5aea846db12e47841
Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>
2021-08-03 20:20:06 +0200

    fix: Skip qbittorrent beta versions
    Signed-off-by: Bᴇʀɴᴅ Sᴄʜᴏʀɢᴇʀs <me@bjw-s.dev>

--- apps/qbittorrent/latest-version.sh
+++ apps/qbittorrent/latest-version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-version=$(curl -sX GET "https://api.github.com/repos/qbittorrent/qBittorrent/tags" | jq --raw-output '.[0].name')
+version=$(curl -sX GET "https://api.github.com/repos/qbittorrent/qBittorrent/tags" | jq --raw-output 'map(select(.name | test("^release-[0-9]+.[0-9]+.[0-9]+$"))) | first.name')
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
